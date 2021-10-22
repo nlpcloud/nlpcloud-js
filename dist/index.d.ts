31e7085721dc555383aa298b4d0b84cf9891cd32
Julien Salinas <all@juliensalinas.com>
2021-10-22 13:35:30 +0000

    Set proper types instead of the default any type.

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,21 +1,95 @@
 export = Client;
 declare class Client {
-    constructor(model: any, token: any, gpu?: boolean);
+    constructor(model: string, token: string, gpu?: boolean);
     headers: {
         Authorization: string;
         'User-Agent': string;
     };
     rootURL: string;
-    entities(text: any): any;
-    classification(text: any, labels: any, multiClass?: any): any;
-    generation(text: any, minLength?: any, maxLength?: any, lengthNoInput?: any, endSequence?: any, removeInput?: any, doSample?: any, numBeams?: any, earlyStopping?: any, noRepeatNgramSize?: any, numReturnSequences?: any, topK?: any, topP?: any, temperature?: any, repetitionPenalty?: any, lengthPenalty?: any, badWords?: any): any;
-    sentiment(text: any): any;
-    question(context: any, question: any): any;
-    summarization(text: any): any;
-    translation(text: any): any;
-    langdetection(text: any): any;
-    tokens(text: any): any;
-    dependencies(text: any): any;
-    sentenceDependencies(text: any): any;
-    libVersions(): any;
+    entities(text: string): Promise<{
+        status: number;
+        statusText: string;
+        data: {
+            entities: { start: number; end: number; type: string; text: string }[];
+        };
+    }>;
+    classification(text: string, labels: string[], multiClass?: boolean): Promise<{
+        status: number;
+        statusText: string;
+        data: {
+            labels: string[];
+            scores: number[];
+        };
+    }>;
+    generation(text: string, minLength?: number, maxLength?: number, lengthNoInput?: boolean, endSequence?: string, removeInput?: boolean, doSample?: boolean, numBeams?: number, earlyStopping?: boolean, noRepeatNgramSize?: number, numReturnSequences?: number, topK?: number, topP?: number, temperature?: number, repetitionPenalty?: number, lengthPenalty?: number, badWords?: string[]): Promise<{
+        status: number;
+        statusText: string;
+        data: {
+            generated_text: string;
+        };
+    }>;;
+    sentiment(text: string): Promise<{
+        status: number;
+        statusText: string;
+        data: {
+            scored_labels: { label: string, score: number }[];
+        };
+    }>;
+    question(context: string, question: string): Promise<{
+        status: number;
+        statusText: string;
+        data: {
+            answer: string;
+            score: number;
+            start: number;
+            end: number;
+        };
+    }>;
+    summarization(text: string): Promise<{
+        status: number;
+        statusText: string;
+        data: {
+            summary_text: string;
+        };
+    }>;
+    translation(text: string): Promise<{
+        status: number;
+        statusText: string;
+        data: {
+            translation_text: string;
+        };
+    }>;
+    langdetection(text: string): Promise<{
+        status: number;
+        statusText: string;
+        data: {
+            languages: any[];
+        };
+    }>;
+    tokens(text: string): Promise<{
+        status: number;
+        statusText: string;
+        data: {
+            tokens: { start: number, end: number, index: number, text: string, lemma: string, ws_after: boolean }[];
+        };
+    }>;
+    dependencies(text: string): Promise<{
+        status: number;
+        statusText: string;
+        data: {
+            words: { text: string, tag: string }[];
+        };
+    }>;
+    sentenceDependencies(text: string): Promise<{
+        status: number;
+        statusText: string;
+        data: {
+            sentence_dependencies: { sentence: string, dependencies: { words: { text: string, tag: string }[], arcs: { start: number, end: number, label: string, text: string, dir: string }[] } }[];
+        };
+    }>;
+    libVersions(): Promise<{
+        status: number;
+        statusText: string;
+        data: any;
+    }>;
 }
